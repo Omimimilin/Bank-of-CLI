717dbf8318c6101696867a2bc28694fe49997038
Naomi <naomilin4668@gmail.com>
2026-09-17 09:43:12 -0500

    connected to database!

--- bank-of-cli/src/main/java/com/bankofcli/persistence/ConnectionFactory.java
+++ bank-of-cli/src/main/java/com/bankofcli/persistence/ConnectionFactory.java
@@ -14,7 +14,7 @@ public class ConnectionFactory {
 
     private ConnectionFactory() {
         try {
-            props.load(new FileReader("src/main/resource/db.properties"));
+            props.load(new FileReader("src/main/resources/db.properties"));
         } catch (IOException e) {
             e.printStackTrace();
         }
